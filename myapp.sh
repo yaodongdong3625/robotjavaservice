7c5dd1135241937565defd46ea197a7ddd156bf6
yaodong <846559623@qq.com>
2018-12-29 13:08:50 +0800

    add new jar and myapp.sh

---
+++ myapp.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+#
+### 配置路径
+APP_DIR=/test
+#//配置名称
+APP_NAME=myapp
+APP_CONF=$APP_DIR/application.properties
+
+#set java home
+export JAVA_HOME=/opt/jdk1.8.0_111
+
+usage() {
+    echo "Usage: sh eju-micro-app.sh [start|stop|deploy]"
+    exit 1
+}
+
+kills(){
+    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    if [[ $tpid ]]; then
+        echo 'Kill Process!'
+        kill -9 $tpid
+    fi
+}
+
+start(){
+    rm -f $APP_DIR/tpid
+    #nohup java -jar myapp.jar --spring.config.location=application.yml > /dev/null 2>&1 &
+    nohup java -jar $APP_DIR/"$APP_NAME".jar > /dev/null 2>&1 &
+    echo $! > $APP_DIR/tpid
+    echo Start Success!
+}
+
+stop(){
+        tpid1=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    echo tpid1-$tpid1
+        if [[ $tpid1 ]]; then
+        echo 'Stop Process...'
+        kill -15 $tpid1
+    fi
+    sleep 5
+    tpid2=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+        echo tpid2-$tpid2
+    if [[ $tpid2 ]]; then
+        echo 'Kill Process!'
+        kill -9 $tpid2
+    else
+        echo 'Stop Success!'
+    fi
+
+}
+
+check(){
+    tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+    if [[ tpid ]]; then
+        echo 'App is running.'
+    else
+        echo 'App is NOT running.'
+    fi
+}
+
+deploy() {
+    kills
+    rm -rf $APP_DIR/"$APPNAME".jar
+    cp "$APP_NAME".jar $APP_DIR
+}
+
+case "$1" in
+    "start")
+        start
+        ;;
+    "stop")
+        stop
+        ;;
+    "kill")
+        kills
+        ;;
+    "deploy")
+        deploy
+        ;;
+    *)
+        usage
+        ;;
+esac
